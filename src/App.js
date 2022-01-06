9968ce7b48f751b6c5c3d996c63614627ebaeb3c
canosaer <canosaer@gmail.com>
2022-01-06 11:33:12 -0500

    hw 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const data = {
+  src: 'https://www.fillmurray.com/640/320',
+  width: 640,
+  height: 320,
+  alt: 'Bill Murray',
+  caption: `"Whatever you do, always give 100%. Unless you're donating blood."`,
+}
+
+
+const HelloWorld = ({src, width, height, alt, caption}) =>{
+  const [count, setCount] = useState(0);
+
+  const buttonClicked = () => {
+    setCount(count + 1)
+  }  
+
+  return(
+    <figure>
+      <img src={src} width={width} height={height} alt={alt} />
+      <figcaption>
+        {caption}
+      </figcaption>
+      <button onClick={buttonClicked}>Click Me! {count} </button>
+    </figure>
+  )
+}
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld {...data}/>
     </div>
   );
 }
